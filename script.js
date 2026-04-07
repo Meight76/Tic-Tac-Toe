1880dd1284d866fed132e46a5075f22bfbd416e4
Luiz <luizribeirocorreia09@gmail.com>
2026-04-07 15:14:47 -0300

    Start game function
    Create some private variables for controlling state
    Create basic functions to handle game states
    Add playTurn features

---
+++ script.js
@@ -0,0 +1,67 @@
+function players(name, symbol, number) {
+    let isWinner = false;
+return {name, symbol, isWinner, number};
+}
+// add a function in order to create private functions and variables
+let game = (() => {
+    let result = null;
+    let roundCount = 0;
+    let isThereAWinner = false;
+    let currentPlayerTurn = player1;
+    let board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    let player1;
+    let player2;
+
+    // use the player object passed as parameter and play the round for him
+    // return the next player's turn
+    const playTurn = (player) => {
+        const answer = prompt(`Player${player.number} where to play ? ([row] [column])`);
+        const playSpot = answer.split(" ");
+        board[playSpot[0] - 1][playSpot[1] - 1] = player.symbol;
+
+        if (player.number === 1) {
+            return player2;
+        } else {
+            return player1;
+        }
+    };
+
+    const gameResult = () => {
+
+    };
+
+    const finishGame = () => {
+
+    };
+
+    const playGame = () => {
+        if (player1 === undefined || player1 === null) {
+            player1 = players(prompt("Player 1 Name:"), "X", 1);
+            player2 = players(prompt("Player 2 Name:"), "O", 2);
+        }
+
+        while (!isThereAWinner) {
+            currentPlayerTurn = playTurn(currentPlayerTurn);
+
+            if (roundCount >= 3) {
+
+                if (gameResult()) {
+                    finishGame();
+                }
+            }
+        }
+    }
+    return {playGame, result}
+})();
+// first i'll get the user input from console
+// in other words one player's turn at a time
+
+// then i'll computate the gameboard into an array
+// that where i'm saving and updating
+
+// after the third round i must start checking for a winner
+// i shall check all rows
